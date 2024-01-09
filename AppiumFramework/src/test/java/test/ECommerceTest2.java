cdde57fb6becc5effc9f2dd197f079ff2c5b6a9a
Dank <Dank@DESKTOP-K2QB92J>
2024-01-09 17:18:33 +0000

    s

--- AppiumFramework/src/test/java/test/ECommerceTest2.java
+++ AppiumFramework/src/test/java/test/ECommerceTest2.java
@@ -8,7 +8,7 @@ public class ECommerceTest2 extends BaseTest {
 
 	@Test
 	public void fillForm() throws InterruptedException {
-		double sum=0.0;
+		double sum = 0.0;
 
 		driver.findElement(
 				By.xpath("//android.widget.EditText[@resource-id='com.androidsample.generalstore:id/nameField']"))
@@ -17,26 +17,32 @@ public class ECommerceTest2 extends BaseTest {
 		driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
 
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		
-		driver.findElement(By.xpath("(//android.widget.TextView[@resource-id='com.androidsample.generalstore:id/productAddCart'])[1]")).click();
-		driver.findElement(By.xpath("(//android.widget.TextView[@resource-id='com.androidsample.generalstore:id/productAddCart'])[2]")).click();
-		
+
+		driver.findElement(By.xpath(
+				"(//android.widget.TextView[@resource-id='com.androidsample.generalstore:id/productAddCart'])[1]"))
+				.click();
+		driver.findElement(By.xpath(
+				"(//android.widget.TextView[@resource-id='com.androidsample.generalstore:id/productAddCart'])[2]"))
+				.click();
+
 		driver.findElement(By.xpath(
 				"//android.widget.ImageButton[@resource-id='com.androidsample.generalstore:id/appbar_btn_cart']"))
 				.click();
-		
+
 		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
-		for(int i=0;i<count;i++) {
-		String priceText = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i).getText();
-		double price = Double.parseDouble(priceText.substring(1,priceText.length()-1));
-		 sum = sum+price;
-		
+		for (int i = 0; i < count; i++) {
+			String priceText = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i)
+					.getText();
+			double price = Double.parseDouble(priceText.substring(1, priceText.length() - 1));
+			sum = sum + price;
+
 		}
-		
-		double lastPageTotal = Double.parseDouble(driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl"))
-				.getText().substring(2,driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText().length()-1));
+
+		double lastPageTotal = Double.parseDouble(
+				driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText().substring(2,
+						driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText().length()
+								- 1));
 		Assert.assertEquals(lastPageTotal, sum);
-		//testgit1
 
 	}
 }
